a7cc63890510ef53df65b6f11ae5f85ecfb622ee
nhanifah <119664502+nhanifah@users.noreply.github.com>
2024-02-15 14:35:50 +0700

    feat: login controller

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import { store } from '../stores/store'
 import { Provider } from 'react-redux'
 import { Toaster } from "react-hot-toast";
 import '../css/main.css'
+import { SessionProvider } from 'next-auth/react';
 
 export type NextPageWithLayout<P = Record<string, unknown>, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -21,64 +22,52 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page)
 
-  const title = `Admin One React Tailwind free`
+  const title = `LPK Harehare Indonesia`
 
-  const description = 'Admin One - free React Next Tailwind dashboard with TypeScript and dark mode'
+  const description = 'LPK Dashboard management system.'
 
-  const url = 'https://justboil.github.io/admin-one-react-tailwind/'
+  const url = 'https://lpk-harehare.id/'
 
-  const image = `https://static.justboil.me/templates/one/repo-tailwind-react.png`
+  const image = `https://cdn.lpk-harehare.id/lpk_harehare.png`
 
   const imageWidth = '1920'
 
   const imageHeight = '960'
 
   return (
-    <Provider store={store}>
-      {getLayout(
-        <>
-          <Head>
-            <meta name="description" content={description} />
+    <SessionProvider>
+      <Provider store={store}>
+        {getLayout(
+            <>
+              <Head>
+                <meta name="description" content={description} />
 
-            <meta property="og:url" content={url} />
-            <meta property="og:site_name" content="JustBoil.me" />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:image" content={image} />
-            <meta property="og:image:type" content="image/png" />
-            <meta property="og:image:width" content={imageWidth} />
-            <meta property="og:image:height" content={imageHeight} />
+                <meta property="og:url" content={url} />
+                <meta property="og:site_name" content="LPK Harehare Indonesia" />
+                <meta property="og:title" content={title} />
+                <meta property="og:description" content={description} />
+                <meta property="og:image" content={image} />
+                <meta property="og:image:type" content="image/png" />
+                <meta property="og:image:width" content={imageWidth} />
+                <meta property="og:image:height" content={imageHeight} />
 
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:title" content={title} />
-            <meta property="twitter:description" content={description} />
-            <meta property="twitter:image:src" content={image} />
-            <meta property="twitter:image:width" content={imageWidth} />
-            <meta property="twitter:image:height" content={imageHeight} />
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:title" content={title} />
+                <meta property="twitter:description" content={description} />
+                <meta property="twitter:image:src" content={image} />
+                <meta property="twitter:image:width" content={imageWidth} />
+                <meta property="twitter:image:height" content={imageHeight} />
 
-            <link rel="icon" href="/admin-one-react-tailwind/favicon.png" />
-          </Head>
+                <link rel="icon" href="https://cdn.lpk-harehare.id/lpk_harehare.png" />
+              </Head>
 
-          <Toaster />
+              <Toaster />
 
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=UA-130795909-1"
-            strategy="afterInteractive"
-          />
-
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'UA-130795909-1');
-            `}
-          </Script>
-
-          <Component {...pageProps} />
-        </>
-      )}
-    </Provider>
+              <Component {...pageProps} />
+            </>
+        )}
+      </Provider>
+    </SessionProvider>
   )
 }
 
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -10,7 +10,10 @@ import Divider from '../components/Divider'
 import Buttons from '../components/Buttons'
 import { useRouter } from 'next/router'
 import { getPageTitle } from '../config'
-import { mdiGoogle } from '@mdi/js'
+import { mdiGoogle, mdiLogin } from '@mdi/js'
+import {signIn, useSession} from "next-auth/react";
+import { toast } from "react-hot-toast";
+import { useState } from "react";
 
 type LoginForm = {
   email: string
@@ -19,12 +22,58 @@ type LoginForm = {
 }
 
 const LoginPage = () => {
+  const { data: session } = useSession()
   const router = useRouter()
+  const [showLoaderCredentials, setShowLoaderCredentials] = useState(false);
+  const [showLoaderGoogle, setShowLoaderGoogle] = useState(false);
+  const [userInfo, setUserInfo] = useState({ email: "", password: "" });
 
-  const handleSubmit = (formValues: LoginForm) => {
-    router.push('/dashboard')
-    console.log('Form values', formValues)
+  const handleSubmit = (e) => {
+    // router.push('/dashboard')
+    e.preventDefault();
+    setShowLoaderCredentials(true);
+    console.log(userInfo);
+    setTimeout(() => {
+      setShowLoaderCredentials(false);
+    }, 1000);
+
+    // validate user credentials
+    // if valid, redirect to dashboard
+    // else, show error message
+    signIn('credentials', {
+        email: userInfo.email,
+        password: userInfo.password,
+        redirect: false,
+    }).then(({ok, error}) => {
+        if (ok) {
+            router.push('/dashboard');
+        } else {
+            console.log(error);
+            toast.error(error);
+        }
+    })
   }
+
+  const handleGoogleLogin = (e) => {
+    e.preventDefault();
+    setShowLoaderGoogle(true);
+    console.log(userInfo);
+    setTimeout(() => {
+      setShowLoaderGoogle(false);
+    }, 3000);
+  }
+
+  if (session) {
+    router.push('/dashboard')
+  }
+
+  const handleChangeEmail = (event) => {
+    setUserInfo({ email: event.target.value, password: userInfo.password });
+  }
+
+    const handleChangePassword = (event) => {
+        setUserInfo({ email: userInfo.email, password: event.target.value });
+    }
 
   const initialValues: LoginForm = {
     email: null,
@@ -55,11 +104,19 @@ const LoginPage = () => {
                     name="email"
                     className="text-2xl"
                     placeholder="Surat Elektronik / Nama Pengguna"
+                    onChange={handleChangeEmail}
+                    value={userInfo.email}
                   />
                 </FormField>
 
                 <FormField label="Katasandi">
-                  <Field name="password" type="password" placeholder="Katasandi" />
+                  <Field
+                      name="password"
+                      type="password"
+                      placeholder="Katasandi"
+                      onChange={handleChangePassword}
+                      value={userInfo.password}
+                  />
                 </FormField>
 
                 <Divider />
@@ -68,8 +125,13 @@ const LoginPage = () => {
                   <Button
                     type="submit"
                     label="Masuk"
+                    icon={mdiLogin}
+                    iconSize={24}
+                    loading={showLoaderCredentials}
                     color="void"
                     className="bg-main-500 text-white px-20 py-3 text-xl rounded-lg font-semibold w-full"
+                    onClick={handleSubmit}
+                    disabled={showLoaderCredentials}
                   />
                 </Buttons>
                 <div className="relative flex py-5 items-center">
@@ -81,6 +143,10 @@ const LoginPage = () => {
                   <Button
                     type="submit"
                     icon={mdiGoogle}
+                    label="Google"
+                    loading={showLoaderGoogle}
+                    onClick={handleGoogleLogin}
+                    disabled={showLoaderGoogle}
                     color="void"
                     iconSize={24}
                     className=" border border-main-500 text-main-500 px-20 py-3 text-xl rounded-lg font-semibold w-full"
