e9a730425cea9952de007b6004610ba85f235019
nhanifah <119664502+nhanifah@users.noreply.github.com>
2024-02-20 16:58:31 +0700

    feat: student page

---
+++ src/pages/student/list.tsx
@@ -0,0 +1,43 @@
+import { mdiAccount, mdiBallotOutline, mdiGithub, mdiMail, mdiUpload } from '@mdi/js'
+import { Field, Form, Formik } from 'formik'
+import Head from 'next/head'
+import React, { ReactElement } from 'react'
+import Button from '../../components/Button'
+import Buttons from '../../components/Buttons'
+import Divider from '../../components/Divider'
+import CardBox from '../../components/CardBox'
+import FormCheckRadio from '../../components/Form/CheckRadio'
+import FormCheckRadioGroup from '../../components/Form/CheckRadioGroup'
+import FormField from '../../components/Form/Field'
+import FormFilePicker from '../../components/Form/FilePicker'
+import LayoutAuthenticated from '../../layouts/Authenticated'
+import SectionMain from '../../components/Section/Main'
+import SectionTitle from '../../components/Section/Title'
+import SectionTitleLineWithButton from '../../components/Section/TitleLineWithButton'
+import { getPageTitle } from '../../config'
+import TableSampleClients from "../../components/Table/SampleClients";
+
+const StudentList = () => {
+    return (
+        <>
+            <Head>
+                <title>{getPageTitle('Daftar Siswa')}</title>
+            </Head>
+
+            <SectionMain>
+                <SectionTitleLineWithButton icon={mdiBallotOutline} title="Daftar Siswa" main>
+                </SectionTitleLineWithButton>
+
+                <CardBox className="mb-6" hasTable>
+                    <TableSampleClients />
+                </CardBox>
+            </SectionMain>
+        </>
+    )
+}
+
+StudentList.getLayout = function getLayout(page: ReactElement) {
+    return <LayoutAuthenticated>{page}</LayoutAuthenticated>
+}
+
+export default StudentList
